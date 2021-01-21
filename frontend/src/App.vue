8490a835a3423dee2d17368beedcde55182dc5e7
TAKURO FUKAMIZU <takuro.f.1987@gmail.com>
2021-01-22 02:26:10 +0900

    use quaser

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,42 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+    q-layout(view="lHh Lpr lFf")
+      q-header.glossy(elevated="")
+        q-toolbar
+          q-btn(flat="" dense="" round="" @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu" icon="menu")
+            q-toolbar-title
+              | MIDI Conv
+            //- div Quasar v{{ $q.version }}
+      q-drawer(v-model="leftDrawerOpen" show-if-above="" bordered="" content-class="bg-grey-2")
+        q-list
+          q-item-label(header="") Essential Links
+          q-item(clickable="" tag="a" target="_blank" href="https://quasar.dev")
+            q-item-section(avatar="")
+              q-icon(name="school")
+            q-item-section
+              q-item-label Docs
+              q-item-label(caption="") quasar.dev
+          q-item(clickable="" tag="a" target="_blank" href="https://github.com/quasarframework/")
+            q-item-section(avatar="")
+              q-icon(name="code")
+            q-item-section
+              q-item-label Github
+              q-item-label(caption="") github.com/quasarframework
+      q-page-container
+        hello-world
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue'
+
+@Component({
+  components: { HelloWorld }
+})
+export default class App extends Vue {
+  private name = 'LayoutDefault';
+  private leftDrawerOpen = false;
+}
+</script>
 
 <style lang="stylus">
 #app
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,61 +1,112 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+  q-page.flex.flex-center
+    div
+      q-btn(color="primary" label="Load Devices" icon="cached" @click="listDevices")
+    q-separator
+    q-card
+      q-tabs.bg-primary.text-white.shadow-2(v-model="deviceTab" align="justify" narrow-indicator)
+        q-tab(name="inputs" icon="input" label="Input")
+        q-tab(name="outputs" icon="outbond" label="Output")
+      q-separator
+      q-tab-panels(v-model="deviceTab" animated)
+        q-tab-panel(name="inputs")
+          .text-h6 MIDI Input Devices
+          q-list(bordered separator)
+            q-item(v-for="input in inputs" :key="input.device.id" clickable v-ripple)
+              q-item-section
+                q-item-label {{input.device.name}}
+                q-item-label(caption) {{input.device.manufacturer}} 
+              q-item-section(side)
+                q-toggle(color="blue" v-model="input.use" val="use")
+        q-tab-panel(name="outputs")
+          .text-h6 MIDI Output Devices
+          q-list(bordered separator)
+            q-item(v-for="output in outputs" :key="output.device.id" clickable v-ripple)
+              q-item-section
+                q-item-label {{output.device.name}}
+                q-item-label(caption) {{output.device.manufacturer}} 
+              q-item-section(side)
+                q-toggle(color="blue" v-model="output.use" val="use")
+    q-card
+      q-list(separator)
+        q-item(v-for="msg in messages" :key="msg.timeStamp") {{msg.srcElement.name}} > {{msg.data}}
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { MIDIInput, MIDIOutput, MIDIMessageEvent } from 'webmidi';
+
+interface MidiDeviceWithConditionInterface {
+  kind: 'input' | 'output';
+  device: MIDIInput | MIDIOutput;
+  use: boolean;
+}
+
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  private deviceTab = 'inputs';
+
+  private inputs: MidiDeviceWithConditionInterface[] = [];
+  private outputs: MidiDeviceWithConditionInterface[] = [];
+  private messages: MIDIMessageEvent[] = [];
+
+  private async listDevices() {
+    this.inputs = [];
+    this.outputs = [];
+
+    // const access = await navigator.requestMIDIAccess({ sysex: false })
+    const access = await navigator.requestMIDIAccess();
+    access.onstatechange = (e) => {
+      // Print information about the (dis)connected MIDI controller
+      console.info(e.port.name, e.port.manufacturer, e.port.state);
+      // TODO: 変更を動的に変更
+    }
+
+    // MIDI Inputデバイスの配列を作成
+    const inputIterator = access.inputs.values();
+    for (let o = inputIterator.next(); !o.done; o = inputIterator.next()) {
+    // for (const o of inputIterator) {
+      console.log(o.value);
+      o.value.onmidimessage = this.onMidiMessageEvent;
+      this.inputs.push({
+        kind: 'input',
+        device: o.value,
+        use: false
+      } as MidiDeviceWithConditionInterface);
+    }
+
+    // MIDI Outputデバイスの配列を作成
+    const outputIterator = access.outputs.values();
+    for (let o = outputIterator.next(); !o.done; o = outputIterator.next()) {
+    // for (const o of outputIterator) {
+      console.log(o.value);
+      this.outputs.push({
+        kind: 'output',
+        device: o.value,
+        use: false
+      } as MidiDeviceWithConditionInterface);
+    }
+  }
+
+  private onMidiMessageEvent(event: MIDIMessageEvent) {
+    // MIDIメッセージを表示
+    console.log(event)
+    const fromDeviceId = event.srcElement.id;
+    for (const { device, use } of this.inputs) {
+      if (device.id != fromDeviceId) continue;
+      if (!use) return; // 使用しない
+    }
+
+    this.messages.unshift(event)
+    for (const { device, use } of this.outputs) {
+      if (!use) continue;
+      device.send(event.data);
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
 </style>
